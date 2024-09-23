e8e56adf20eafe2a205a5802701f750f0748509e
Anand Babu <anandbabupanackal2003@gmail.com>
2024-09-23 12:14:52 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,56 @@
+
+<LinearLayout xmlns:android="http//schemas.android.com/apk/res/android"
+  android:layout_width="fill_parent"
+  android:layout_height="fill_parent"
+  android:orientation="horizontal">
+   <LinearLayout
+    android:layout_width="fill_parent"
+    android:layout_height="fill_parent"
+    android:layout_orientation="vertical"
+>
+   <Button
+   android:id="dafa3"
+   android:text="anand"
+    android:layout_width="wrap_content"
+   android:layout_width="fill_parent"/>
+
+   <TextView
+   android:id="aff";
+   android:text="anand"
+   android:background="#2312"
+   android:layout_width="fill_parent"
+    android:layout_height="wrap content"
+   />
+ <AbsoluteLayout
+ android:layout_width="fill_parent"
+ android:layout_height="fill_parent"
+>
+   <Button
+   android:id="dafa3"
+   android:text="anand"
+   android:layout_x ="89px"
+    android:layout_y ="389px"
+    android:layout_width="wrap_content"
+   android:layout_width="fill_parent"/>
+
+
+</AbsoluteLayout>
+<FrameLayout>
+android:layout_alignLeft="DWD"
+android:layout_alignParentBottom=""
+</FrameLayout>
+<RelativeLayout>
+
+android:layout_alignParentBottom="true"
+android:layout_alignParentLeft="true"
+</RelativeLayout>
+  </LinearLayout>
+  <Button
+android:layout_width="fill_content"
+android:layout_alignParentTop="true"
+android:layout_centerHorizontal="true"
+android:id=""
+</LinearLayout>
+
+
+ 
